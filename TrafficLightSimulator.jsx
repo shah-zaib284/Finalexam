22cd75cca610a60527b064d318cef902f67f5cd6
shah-zaib284 <shahzaibhussain284@gmail.com>
2025-06-03 11:33:33 +0500

    1st commit
    adding bit html , css architecture

---
+++ TrafficLightSimulator.jsx
@@ -0,0 +1,33 @@
+import { Component, useState } from "react"
+import { useEffect } from "react"
+import { useState } from 'react'
+
+
+Component
+function App() {
+  const [count, setCount] = useState(1, 3000);
+  useEffect(color : Yellow )
+
+useState(col)
+<button>id=1 Red <style>
+  
+  
+  <style>
+    
+    id : 1, {color: Red} ;
+  id: 2 { color : Yellow};
+  id : 3 {color : Green };
+
+
+</style>
+
+
+
+ <body>
+  onclick(getElementbyId(1)
+color: useEffect())
+  
+ </body> </button>
+
+
+
